faff41ac6317ddc880200b5acb11ad9a6984a022
Adminsvu <Baikonur96@yandex.ru>
2024-02-28 16:40:55 +0300

    v.1.2

--- src/main/java/by/exp/springStudents/controller/StudentController.java
+++ src/main/java/by/exp/springStudents/controller/StudentController.java
@@ -1,15 +1,24 @@
 package by.exp.springStudents.controller;
 
+import by.exp.springStudents.model.Student;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController //ResponseBody and формирует ответ в JSON
 @RequestMapping("api/v1/students")
 public class StudentController {
 
-
-    public List<Student> {
-        return
+    @GetMapping
+    public List<Student> findAllStudent() {
+        return List.of(
+                Student.builder().firstName("Oleg").email("oleg12@lol.com").age(24).build(),
+                Student.builder().firstName("Alex").email("alex@lol.com").age(28).build(),
+                Student.builder().firstName("John").email("john12@lol.com").age(19).build()
+        );
     }
 }
--- src/main/java/by/exp/springStudents/model/Student.java
+++ src/main/java/by/exp/springStudents/model/Student.java
@@ -1,16 +1,11 @@
 package by.exp.springStudents.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
+@Builder
 public class Student {
     private String firstName;
     private String lastname;
